f3062e83425dd3310c267e3d9ac642a8c779e62c
vilmar <vilmarmoreira2010@gmail.com>
2021-03-28 10:51:09 -0300

    adição readme.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# buscado cep
+
+<a href="https://vilmar-moreira-souza.github.io/buscador-cep/" target="blank">visualizar</a>
+
+
+<h2>desktop</h2>
+<img style=border:solid src="./imagens/Captura0.png" width="" alt="print da tela">
+
+<h2>mobile</h2>
+<img  style=border:solid src="./imagens/Captura1.png" width="200" alt="print da tela">
+<br>
+
+<h3>sobre a aplicação</h3>
+<p>essa aplicação é um pagina para procura de cep atraves do codigo(numeros), usando a  <a href="https://docs.awesomeapi.com.br/api-cep">api awesomeap </a> .</p>
+ <a href="https://vilmar-moreira-souza.github.io/buscador-cep/" target="blank">visualizar </a>
+ 
+
+<h3>ideias para implementar</h3>
+ <li>melhorar o design</li>
+<li> adicionar busca pelo nome da rua</li>
+<li>mostrar formato esperado (00000-000), enquanto usuario digita </li>
+<li>limitar o numero de caracteres digitados, modelo esperado (00000000)</li>
+<li> mais...</li>
+
+<h3>bugs e problemas</h3>
+<li>...</li>
--- javascript/app.js
+++ javascript/app.js
@@ -5,39 +5,51 @@ const entrada = document.querySelector('[data-js="campoInput"]')
 const pesquisar = document.querySelector('[data-js="campoButton"]')
 const resultado = document.querySelector('.busca').children[2]
 const historico = document.querySelector('.lateral')
+const historicoMobile = document.querySelector('[data-js="spanMobile"]')
+historicoMobile.innerHTML += `<br><br><h2 class="noVisible" >historico</h2> `
 
-
+// busca cep 
 function buscaCep() {
-    if (entrada.value !="") {
+    if (entrada.value != "") {
         fetch(`https://cep.awesomeapi.com.br/${entrada.value}`)
-        .then((data)=>{
-           return data.json()
-        })
-        .then((cep)=>{
-            //console.log(cep.status)
-            if (cep.status ==  404) {
-                resultado.innerHTML =`<br><hr><h2 class="alert">  erro</h2> `+ cep.message
-            }
-            
-            if (cep.status ==  400) {
-                resultado.innerHTML =`<br><hr><h2 class="alert"> nao encontrado</h2> `+ cep.message
-            }
-            else if (cep.status != 404 && cep.status !=400) {
-                           
-            let acumulador =`<hr>cep:${cep.cep} <br> cidade:${cep.city} <br>${cep.address}<br>estado: ${cep.state}`
-            historico.innerHTML += acumulador  
-            resultado.innerHTML = acumulador
-        }
-        })
-        .then((err)=>{
-            console.log(err)
-        })
+            .then((data) => {
+                return data.json()
+            })
+            .then((cep) => {
+                //console.log(cep)
+                if (cep.status == 404) {
+                    resultado.innerHTML = `<br><hr><h2 class="alert">  erro</h2> ` + cep.message
+                }
+
+                if (cep.status == 400) {
+                    resultado.innerHTML = `<br><hr><h2 class="alert"> nao encontrado</h2> ` + cep.message
+                }
+                else if (cep.status != 404 && cep.status != 400) {
+                    //console.log(cep.status)              
+                    let acumulador = `<hr>cep:${cep.cep} <br> cidade:${cep.city}- ${cep.state} <br>${cep.address} <br>Bairro: ${cep.district}`
+                    historico.innerHTML += acumulador
+                    resultado.innerHTML = acumulador
+                    historicoMobile.innerHTML += `<div class="noVisible"> ${acumulador} <div>`
+
+                }
+            })
+            .then((err) => {
+                console.log(err)
+            })
         //entrada.value=""
     }
-    else{
-        resultado.innerHTML = `<br><hr> <h2 class="alert">digite um cep</h2>`
+    else {
+        resultado.innerHTML = `<br><hr> <h2 class="alert ">digite um cep</h2>`
     }
-    
+
 }
 
-pesquisar.addEventListener("click",buscaCep)
+//procurar cep quando tecla enter for clicada
+const enterCep = (event) => {
+    if (event.key == "Enter") {
+        buscaCep()
+    }
+}
+
+entrada.addEventListener("keypress", enterCep)
+pesquisar.addEventListener("click", buscaCep)
